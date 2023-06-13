817ca58cd097b2d095f1526dde2ec5c80237ca62
aravinthraajk <aravinthraajk@gmail.com>
2023-06-14 00:48:34 +0530

    build: mantine global setup added

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,4 @@
-import type { UserConfig } from '@commitlint/types';
+import type { UserConfig } from '@commitlint/types'
 // build: Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
 // ci: Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
 // docs: Documentation only changes
@@ -48,6 +48,6 @@ const Configuration: UserConfig = {
       ],
     ],
   },
-};
+}
 
-module.exports = Configuration;
+module.exports = Configuration
