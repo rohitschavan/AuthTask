721b446ba3bb212bde85b87725672179ca42a400
aravinthraajk <aravinthraajk@gmail.com>
2023-06-14 13:50:59 +0530

    style: story added for text

---
+++ src/common/components/atoms/Text/Text.stories.ts
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Text } from '@mantine/core'
+
+const meta: Meta<typeof Text> = {
+  title: 'Atoms/Text',
+  component: Text,
+  argTypes: {
+    size: {
+      options: ['xxs', 'xs', 'sm', 'md', 'lg', 'xl', 'xxl'],
+      control: { type: 'select' },
+    },
+  },
+  tags: ['autodocs'],
+}
+
+export default meta
+type Story = StoryObj<typeof Text>
+
+export const Xxs: Story = {
+  args: {
+    children: 'Extra small',
+    size: 'xxs',
+  },
+}
+
+export const Xs: Story = {
+  args: {
+    children: 'Extra small',
+    size: 'xs',
+  },
+}
+
+export const Sm: Story = {
+  args: {
+    children: 'Small',
+    size: 'sm',
+  },
+}
+
+export const Md: Story = {
+  args: {
+    children: 'Medium',
+    size: 'md',
+  },
+}
+
+export const Lg: Story = {
+  args: {
+    children: 'Large',
+    size: 'lg',
+  },
+}
+export const Xl: Story = {
+  args: {
+    children: 'Extra Large',
+    size: 'xl',
+  },
+}
+
+export const Xxl: Story = {
+  args: {
+    children: 'Extra Large',
+    size: 'xxl',
+  },
+}
