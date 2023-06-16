8f6f5e057b47284845b9df076d83c9f2b3e21ef0
aravinthraajk <aravinthraajk@gmail.com>
2023-06-16 10:37:51 +0530

    refactor: typesfoler added

--- src/common/components/atoms/Input/Input.tsx
+++ src/common/components/atoms/Input/Input.tsx
@@ -2,12 +2,6 @@ import React from 'react'
 import { TextInput } from '@mantine/core'
 import './Input.scss'
 
-interface InputTypes {
-  placeholder: string
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-  label: string
-}
-
 const Input = ({ placeholder, onChange, label }: InputTypes) => {
   return (
     <TextInput
