184c438c5868b05b7118a36b11e4a1f749b65e3c
aravinthraajk <aravinthraajk@gmail.com>
2023-06-16 09:47:20 +0530

    refactor: styles changed to scss

--- src/common/components/atoms/Input/Input.stories.ts
+++ src/common/components/atoms/Input/Input.stories.ts
@@ -6,10 +6,7 @@ const meta: Meta<typeof Input> = {
   title: 'Atoms/Input',
   component: Input,
   argTypes: {
-    radius: {
-      options: ['xs', 'sm', 'md', 'lg', 'xl'],
-      control: { type: 'select' },
-    },
+
   },
   tags: ['autodocs'],
 }
@@ -20,6 +17,5 @@ type Story = StoryObj<typeof Input>
 export const Primary: Story = {
   args: {
     label: 'Primary',
-    radius: 'lg',
   },
 }
--- src/common/components/atoms/Input/Input.tsx
+++ src/common/components/atoms/Input/Input.tsx
@@ -1,35 +1,19 @@
 import React from 'react'
-import { TextInput, createStyles } from '@mantine/core'
+import { TextInput } from '@mantine/core'
+import './Input.scss'
 
 interface InputTypes {
   placeholder: string
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
   label: string
-  radius?: string
 }
 
-const useStyles = createStyles((theme) => ({
-  label: {
-    fontSize: theme.fontSizes.xxs,
-    // will update after the merges of previous branches
-    fontWeight: 700,
-  },
-  input: {
-    borderRadius: theme.radius.xs,
-    fontSize: theme.fontSizes.sm,
-    color: theme.colors.gray[4],
-    // will update after the merges of previous branches
-    fontWeight: 700,
-  },
-}))
 const Input = ({ placeholder, onChange, label }: InputTypes) => {
-  const { classes } = useStyles()
   return (
     <TextInput
       placeholder={placeholder}
       onChange={onChange}
       label={label}
-      classNames={{ label: classes.label, input: classes.input }}
     />
   )
 }
