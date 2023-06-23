3d0a19de6ae097808448940297af516327179a5d
aravinthraajk <aravinthraajk@gmail.com>
2023-06-23 23:53:56 +0530

    feat: app shell implemented

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.scss'
 import { Outlet } from 'react-router-dom'
+import Appshell from '../common/components/templates/AppShell/AppShell'
 
 const App = () => {
   return (
     <div className="App">
-      <Outlet />
+      <Appshell>
+        <Outlet />
+      </Appshell>
     </div>
   )
 }
